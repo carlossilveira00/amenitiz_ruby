610fd233e7da50a6fa1d60343b81682a3a1eabbd
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-03-06 18:29:40 -0100

    Create method to check if the arguments from Item are strings and create method to check if the price is a float.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,17 +14,16 @@ RSpec.describe Item do
     end
 
     it 'raises an error if the name is not a string.' do
-      expect(Item.new(12345, 'ABC123', 9.99)).to raise_error(ArgumentError)
+      expect { Item.new(12345, 'ABC123', 9.99) }.to raise_error(ArgumentError)
     end
 
     it 'raises an error if the product_code is not a string.' do
-      expect(Item.new('Test Item', 123, 9.99)).to raise_error(ArgumentError)
+      expect { Item.new('Test Item', 123, 9.99) }.to raise_error(ArgumentError)
     end
 
     it 'raises an error if the price is not a float.' do
-      expect(Item.new('Test Item', 123, 10)).to raise_error(ArgumentError)
+      expect { Item.new('Test Item', 'ABC123', 10) }.to raise_error(ArgumentError)
     end
-
   end
 
 end
