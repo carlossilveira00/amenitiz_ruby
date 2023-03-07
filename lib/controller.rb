f53b5c89876385e9612bd746013a69d2452dd3a9
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-03-07 21:41:13 -0100

    Create tests for add item, the method should receive an input from the user, search in the array to find an item with that index, if it can find it then proceeds to add it the array eçse it does not add anything to the array. Write the method #add_item_to_cart in the controller, this method provides the list of items available, ask the user for the index of an item he would like to add to their cart and then checks if that input is valid, if it is it adds it to the array if its not it doesn't do anything.

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,10 +1,43 @@
 require 'csv'
 require_relative 'item'
+require_relative 'cart'
 
 class Controller
+  attr_reader :cart
+
   def initialize(csv_file)
     @items = []
+    @cart = Cart.new
     load_csv(csv_file)
+  end
+
+  def list_available_items
+    puts 'This is all the items we have available at the moment!'
+    puts '---------------'
+    @items.each_with_index do |item, index|
+      puts "#{index} - [#{item.product_code}] - #{item.name} : #{item.price}$ "
+    end
+    puts '---------------'
+  end
+
+  def add_item_to_cart
+    # List all the items available at the store.
+    list_available_items
+
+    # Ask the user which item he would like to add to the cart.
+    puts 'Please provide us with the index of the item in order to add it to your cart.'
+    wished_item = gets.chomp.to_i
+
+    # Find the item with the index provided and add it to the cart.
+    if @items[wished_item].nil?
+      # If the user provided a wrong index number then provide an error message saying we're unable to find that product.
+      puts "We couldn't find the item you're looking for."
+    else
+      puts '...Beeep...'
+      @cart.add_item(@items[wished_item])
+      # Provide the user with a success message.
+      puts "You're product has been successfully added to your cart!"
+    end
   end
 
   private
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -10,12 +10,42 @@ RSpec.describe Controller do
         expect(controller.instance_variable_get(:@items)).not_to be_nil
       end
 
+      it "sets items instance variable" do
+        expect(controller.instance_variable_get(:@cart)).not_to be_nil
+      end
+
       it "loads the items from the csv." do
         expect(controller.instance_variable_get(:@items)).not_to be_empty
       end
-
     end
   end
+
+  describe "#add_item_to_cart" do
+    before do
+      allow(controller).to receive(:gets).and_return("#{item_index}\n")
+    end
+
+    context 'when the input is valid' do
+      let(:item_index) { 0 }
+
+      it 'adds the item to the cart' do
+        controller.add_item_to_cart
+
+        expect(controller.cart.items).to include(an_instance_of(Item))
+      end
+    end
+
+    context "when the input is invalid" do
+      let(:item_index) { 10 }
+
+      it 'does not add the item to the cart' do
+        controller.add_item_to_cart
+
+        expect(controller.cart.items).to be_empty
+      end
+    end
+  end
+
 
 
 end
