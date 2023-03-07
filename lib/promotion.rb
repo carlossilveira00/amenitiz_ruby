908ccd98618e394209e74be776deb2541dfad7e7
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-03-07 11:40:39 -0100

    Create tests for the #apply method for Promotion, when the promotion is the type of :buy_one_get_one_free it add an new item with the price of 0 with the corrusponding product_code from the promtio, the test also ensures that the promotion is only applied in the correct items. Create #apply method, takes in a array of items as an argument and when the type corresponds to the correct promotion it applies it on the items array. Update price instance variable from Item to be an accessor instead of just a reader, this will be used to change the price to 0 when adding a free item for the promotion.

--- lib/promotion.rb
+++ lib/promotion.rb
@@ -5,4 +5,24 @@ class Promotion
     @type = args[:type]
     @discount = args[:discount]
   end
+
+  def apply(cart)
+    case @type
+    when :buy_one_get_one_free
+      buy_one_get_one_free(cart)
+    end
+  end
+
+  private
+
+  def buy_one_get_one_free(cart_items)
+    matching_items = cart_items.select { |item| item.product_code == @product_code }
+
+    matching_items.each do |item|
+      new_item = item.dup
+      new_item.price = 0
+      cart_items << new_item
+    end
+  end
+
 end
--- spec/promotion_spec.rb
+++ spec/promotion_spec.rb
@@ -3,27 +3,58 @@ require_relative '../lib/cart'
 require_relative '../lib/promotion'
 
 RSpec.describe Promotion do
-  let(:green_tea_promotion) { Promotion.new({ title:'Grean Tea - Buy one and get one free!', product_code: "GR1", type: :buy_one_get_one_free, discount: 100 }) }
+  let(:buy_one_get_one_free_promotion) { Promotion.new({ title:'Grean Tea - Buy one and get one free!', product_code: "GR1", type: :buy_one_get_one_free, discount: 100 }) }
+  let(:item1) { Item.new('Green Tea', 'GR1', 3.50) }
+  let(:item2) { Item.new('Strawberries', 'SR1', 5.00) }
+  let(:item3) { Item.new('Coffee', 'CF1', 11.23) }
+  let(:cart) { Cart.new }
 
   describe "#initialize" do
     context "when creating a new Promotion" do
       it "sets the promotion title" do
-        expect(green_tea_promotion.instance_variable_get(:@title)).to eq('Grean Tea - Buy one and get one free!')
+        expect(buy_one_get_one_free_promotion.instance_variable_get(:@title)).to eq('Grean Tea - Buy one and get one free!')
       end
 
       it "sets the promotion product_code" do
-        expect(green_tea_promotion.instance_variable_get(:@product_code)).to eq("GR1")
+        expect(buy_one_get_one_free_promotion.instance_variable_get(:@product_code)).to eq("GR1")
       end
 
       it "sets the promotion type" do
-        expect(green_tea_promotion.instance_variable_get(:@type)).to eq(:buy_one_get_one_free)
+        expect(buy_one_get_one_free_promotion.instance_variable_get(:@type)).to eq(:buy_one_get_one_free)
       end
 
       it "sets the promotion discount" do
-        expect(green_tea_promotion.instance_variable_get(:@discount)).to eq(100)
+        expect(buy_one_get_one_free_promotion.instance_variable_get(:@discount)).to eq(100)
       end
     end
   end
+
+  describe "#apply" do
+    context "when promotion type is :buy_one_get_one_free" do
+      it "adds an item to the cart but with the price set to 0" do
+        cart.add_item(item1)
+        buy_one_get_one_free_promotion.apply(cart.items)
+
+        expect(cart.items.length).to eq(2)
+        expect(cart.items[1].price).to eq(0)
+      end
+
+      it "applies the promotion to the items with the correct product_code" do
+        cart.add_item(item1)
+        cart.add_item(item1)
+        cart.add_item(item2)
+        cart.add_item(item3)
+
+        buy_one_get_one_free_promotion.apply(cart.items)
+
+        expect(cart.items[2].price).to eq(5.00)
+        expect(cart.items[3].price).to eq(11.23)
+        expect(cart.items[4].price).to eq(0)
+      end
+
+    end
+  end
+
 
 
 
