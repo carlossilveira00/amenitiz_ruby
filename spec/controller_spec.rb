a5a7ae37bae55242d2180793cade3404db04a7d1
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-03-08 16:38:41 -0100

    Update the promotions created in controller_spec to use min_quantity and free_quantity.

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/controller'
 
 RSpec.describe Controller do
-  let(:buy_one_get_one_free_promotion) { Promotion.new({ title:'Grean Tea - Buy one and get one free!', product_code: "GR1", type: :buy_one_get_one_free, discount: 100 }) }
-  let(:price_discount_per_quantity_promotion) { Promotion.new({ title:'Strawberries - Buy 3 or more and get price reduction to 4.50$!', product_code: "SR1", type: :price_discount_per_quantity, discount: 4.50 }) }
-  let(:percentage_discount_per_quantity_promotion) { Promotion.new({ title:'Coffee - Buy 3 or more and get price reduction 2/3 of the price!', product_code: "CF1", type: :percentage_discount_per_quantity, discount: 66.6 }) }
+  let(:buy_one_get_one_free_promotion) { Promotion.new({ title:'Grean Tea - Buy one and get one free!', product_code: "GR1", type: :buy_x_get_x_free, discount: 100, min_quantity: 1, free_quantity: 1 }) }
+  let(:price_discount_per_quantity_promotion) { Promotion.new({ title:'Strawberries - Buy 3 or more and get price reduction to 4.50$!', product_code: "SR1", type: :price_discount_per_quantity, discount: 4.50, min_quantity: 3 }) }
+  let(:percentage_discount_per_quantity_promotion) { Promotion.new({ title:'Coffee - Buy 3 or more and get price reduction 2/3 of the price!', product_code: "CF1", type: :percentage_discount_per_quantity, discount: 66.6, min_quantity: 3 }) }
   let(:promotions) { [buy_one_get_one_free_promotion, price_discount_per_quantity_promotion, percentage_discount_per_quantity_promotion] }
   let(:csv_file) { File.join(__dir__, '../lib/items.csv')  }
   let(:controller) { Controller.new(csv_file, promotions) }
