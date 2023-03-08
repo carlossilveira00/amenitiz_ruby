2b8195408985b9e88c6d3417b1709d890cd2d614
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-03-08 14:33:04 -0100

    Define #run method in the router,it simply displays the actions available to the user, asks for the input and calls #route_action method with the input. Define #display_user_actions, this outputs to the user all the actions available. Define #route_action method, this method takes an argument and depending on the argument it routes to action in the controller.

--- lib/router.rb
+++ lib/router.rb
@@ -3,4 +3,49 @@ class Router
     @controller = controller
     @running = true
   end
+
+  def run
+    puts 'Welcome to Amenitiz Store!'
+    puts "           --           "
+
+    while @running
+      display_user_actions
+      action = gets.chomp.to_i
+      print `clear`
+      route_action(action)
+    end
+  end
+
+  private
+
+  def display_user_actions
+    puts ''
+    puts "What would you like to do next?"
+    puts '1- Display available items.'
+    puts "-----------"
+    puts '2- Add item to cart.'
+    puts "-----------"
+    puts '3- Delete item from cart.'
+    puts "-----------"
+    puts '4- Check cart and checkout.'
+    puts "-----------"
+    puts '5- Exit store.'
+    puts "-----------"
+  end
+
+  def route_action(action)
+    case action
+      when 1 then @controller.list_available_items
+      when 2 then @controller.add_item_to_cart
+      when 3 then @controller.delete_item_from_cart
+      when 4 then @controller.checkout
+      when 5 then stop
+    end
+  end
+
+  def stop
+    puts "What a pleasure to meet you! Hope you come back! :)"
+    @running = false
+  end
+
 end
