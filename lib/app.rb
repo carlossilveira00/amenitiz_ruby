2dde10a7cac1f54dba20a28637ed5d877df29337
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-03-08 14:40:52 -0100

    Fix identation

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,7 @@ require_relative 'cart'
 require_relative 'promotion'
 
 csv_file = File.join(__dir__, 'items.csv')
-promotions = [Promotion.new({title: 'Strawberries Promotion', product_code: 'SR1', type: :price_discount_per_quantity, discount: 4.5}), Promotion.new({title: 'Coffee Promotion', product_code: 'CF1', type: :percentage_discount_per_quantity, discount: 66.3}), Promotion.new({title: 'Green Tea Promotion', product_code: 'GR1', type: :buy_one_get_one_free, discount: 0})]
+promotions = [Promotion.new( { title: 'Strawberries Promotion', product_code: 'SR1', type: :price_discount_per_quantity, discount: 4.5 } ), Promotion.new({title: 'Coffee Promotion', product_code: 'CF1', type: :percentage_discount_per_quantity, discount: 66.3}), Promotion.new({title: 'Green Tea Promotion', product_code: 'GR1', type: :buy_one_get_one_free, discount: 0})]
 controller = Controller.new(csv_file, promotions)
 
 router = Router.new(controller)
